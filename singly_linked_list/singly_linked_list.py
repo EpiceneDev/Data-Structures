204ce6a5bfe67b2f48e57526a5b5449b4d72d65b
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-05-20 19:12:31 -0700

    ssl in progress

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,7 +1,18 @@
-class Node:
+class ListNode:
     def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
+
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        
+    # def insert_before(self, value):
+    #     current_prev = self.prev
+    #     self.prev = ListNode(value, current_prev, self)
+
+    def delete(self):
+        pass
 
 class LinkedList:
     def __init__(self):
@@ -14,8 +25,10 @@ class LinkedList:
     def test_contains(self):
         pass
 
-    def add_to_tail(self):
-        pass
+    def add_to_tail(self, value):
+        if self.head is None:
+            self.head = Node(value, None)
+            return
 
     def remove_head(self):
         pass
